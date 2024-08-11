3d16b419166a74a4879c070b62f45163d04b364e
Lubni Morais <lubni.morais@gmail.com>
2024-08-11 14:04:03 -0300

    bug: error layout animation

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
-import { Alert, Text, View } from 'react-native';
+import { Alert, BackHandler, Text, View } from 'react-native';
 
 import { useNavigation, useRoute } from '@react-navigation/native';
 
@@ -17,6 +17,10 @@ import Animated, {
 } from 'react-native-reanimated';
 
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+
+import { Audio } from 'expo-av';
+
+import * as Haptics from 'expo-haptics';
 
 import { QUIZ } from '../../data/quiz';
 import { historyAdd } from '../../storage/quizHistoryStorage';
@@ -76,6 +80,17 @@ export function Quiz() {
     };
   });
 
+  const playSound = useCallback(async (isCorrect: boolean) => {
+    const file = isCorrect
+      ? require('../../assets/correct.mp3')
+      : require('../../assets/wrong.mp3');
+
+    const { sound } = await Audio.Sound.createAsync(file, { shouldPlay: true });
+
+    await sound.setPositionAsync(0);
+    await sound.playAsync();
+  }, []);
+
   function handleSkipConfirm() {
     Alert.alert('Pular', 'Deseja realmente pular a questão?', [
       { text: 'Sim', onPress: () => handleNextQuestion() },
@@ -112,10 +127,13 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
-      setStatusReply(1);
       setPoints((prevState) => prevState + 1);
+
+      await playSound(true);
+      setStatusReply(1);
       handleNextQuestion();
     } else {
+      await playSound(false);
       setStatusReply(2);
       shakeAnimation();
     }
@@ -139,7 +157,9 @@ export function Quiz() {
     return true;
   }
 
-  function shakeAnimation() {
+  async function shakeAnimation() {
+    await Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
+
     shake.value = withSequence(
       withTiming(3, { duration: 400, easing: Easing.bounce }),
       withTiming(0, undefined, (finished) => {
@@ -239,6 +259,19 @@ export function Quiz() {
       handleNextQuestion();
     }
   }, [points]);
+
+  useEffect(() => {
+    /**
+     * NO SEGUNDO PARÂMETRO PASSAMOS QUAL FUNÇÃO VAI EXECUTAR
+     * QUANDO O BOTÃO VOLTAR NO ANDROID FOR TOCADO
+     */
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      handleStop,
+    );
+
+    return () => backHandler.remove();
+  }, []);
 
   if (isLoading) {
     return <Loading />;
